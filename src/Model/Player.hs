0bb4162845c849a2f31850a7179da882dccf5cef
LER0ever <i+git@rongyi.io>
2021-11-29 16:28:53 -0800

    [fmt] use ormolu to format the whole project
    Signed-off-by: LER0ever <i+git@rongyi.io>

--- src/Model/Player.hs
+++ src/Model/Player.hs
@@ -4,27 +4,34 @@ import Model.Board
 import System.Random -- (Random(randomRIO))
 
 -------------------------------------------------------------------------------
+
 -- | Players and Strategies ---------------------------------------------------
+
 -------------------------------------------------------------------------------
 
-data Player = Player 
-  { plName  :: String 
-  , plStrat :: Strategy
-  } 
+data Player = Player
+  { plName :: String,
+    plStrat :: Strategy
+  }
 
-type Strategy = Pos     -- ^ current cursor
-             -> Board   -- ^ current board
-             -> XO      -- ^ naught or cross
-             -> IO Pos  -- ^ next move
+type Strategy =
+  -- | current cursor
+  Pos ->
+  -- | current board
+  Board ->
+  -- | naught or cross
+  XO ->
+  -- | next move
+  IO Pos
 
-human :: Player 
+human :: Player
 human = Player "human" (\p _ _ -> return p)
 
-rando :: Player 
+rando :: Player
 rando = Player "machine" randomStrategy
 
 randomStrategy :: a -> Board -> b -> IO Pos
-randomStrategy _ b _ = selectRandom (emptyPositions b) 
+randomStrategy _ b _ = selectRandom (emptyPositions b)
 
 selectRandom :: [a] -> IO a
 selectRandom xs = do
--- src/Model/Score.hs
+++ src/Model/Score.hs
@@ -1,17 +1,24 @@
 {-# LANGUAGE RecordWildCards #-}
+
 module Model.Score where
 
 import Model.Board (Result (..), XO (..))
 
 -------------------------------------------------------------------------------
+
 -- | Score --------------------------------------------------------------------
+
 -------------------------------------------------------------------------------
 
-data Score = Score 
-  { scMax  :: Int  -- ^ total number of boards
-  , scX    :: Int  -- ^ points for player X 
-  , scO    :: Int  -- ^ points for player O 
-  , scD    :: Int  -- ^ drawn games 
+data Score = Score
+  { -- | total number of boards
+    scMax :: Int,
+    -- | points for player X
+    scX :: Int,
+    -- | points for player O
+    scO :: Int,
+    -- | drawn games
+    scD :: Int
   }
   deriving (Eq, Ord, Show)
 
@@ -19,27 +26,27 @@ init :: Int -> Score
 init n = Score n 0 0 0
 
 add :: Score -> Maybe XO -> Score
-add sc (Just X) = sc { scX = scX sc + 1 }
-add sc (Just O) = sc { scO = scO sc + 1 }
-add sc Nothing  = sc { scD = scD sc + 1 }
+add sc (Just X) = sc {scX = scX sc + 1}
+add sc (Just O) = sc {scO = scO sc + 1}
+add sc Nothing = sc {scD = scD sc + 1}
 
 get :: Score -> XO -> Int
-get Score {..} X = scX 
-get Score {..} O = scO 
+get Score {..} X = scX
+get Score {..} O = scO
 
 currRound :: Score -> Int
 currRound Score {..} = scX + scO + scD + 1
 
 startPlayer :: Score -> XO
-startPlayer sc 
+startPlayer sc
   | even (currRound sc) = X
-  | otherwise           = O
+  | otherwise = O
 
-winner :: Score -> Result () 
+winner :: Score -> Result ()
 winner sc@Score {..}
   | scX > scO + left = Win X
   | scO > scX + left = Win O
-  | left == 0        = Draw
-  | otherwise        = Cont ()
-  where 
-    left             = 1 + scMax - currRound sc
+  | left == 0 = Draw
+  | otherwise = Cont ()
+  where
+    left = 1 + scMax - currRound sc
