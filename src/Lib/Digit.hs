c65e8dad12297eded385e6fd55c73b65c55f5e5b
LER0ever <i+git@rongyi.io>
2021-12-08 12:14:10 -0800

    [digit] implement time to digital fonts and unit tests
    Signed-off-by: LER0ever <i+git@rongyi.io>

--- src/Lib/Digit.hs
+++ src/Lib/Digit.hs
@@ -1,113 +1,137 @@
-module Lib.Digit () where
+module Lib.Digit
+  ( timeToDigitalLines,
+  )
+where
 
 import Data.Char (digitToInt)
+import Data.List
 
-colon :: String
+colon :: [String]
 colon =
-  unlines
-    [ "..",
-      "#.",
-      "..",
-      "#.",
-      ".."
-    ]
+  [ "..",
+    "#.",
+    "..",
+    "#.",
+    ".."
+  ]
 
-zero :: String
+zero :: [String]
 zero =
-  unlines
-    [ "######.",
-      "#....#.",
-      "#....#.",
-      "#....#.",
-      "######."
-    ]
+  [ "######.",
+    "#....#.",
+    "#....#.",
+    "#....#.",
+    "######."
+  ]
 
-one :: String
+one :: [String]
 one =
-  unlines
-    [ ".....#.",
-      ".....#.",
-      ".....#.",
-      ".....#.",
-      ".....#."
-    ]
+  [ ".....#.",
+    ".....#.",
+    ".....#.",
+    ".....#.",
+    ".....#."
+  ]
 
-two :: String
+two :: [String]
 two =
-  unlines
-    [ "######.",
-      ".....#.",
-      "######.",
-      "#......",
-      "######."
-    ]
+  [ "######.",
+    ".....#.",
+    "######.",
+    "#......",
+    "######."
+  ]
 
-three :: String
+three :: [String]
 three =
-  unlines
-    [ "######.",
-      ".....#.",
-      "...###.",
-      ".....#.",
-      "######."
-    ]
+  [ "######.",
+    ".....#.",
+    "...###.",
+    ".....#.",
+    "######."
+  ]
 
-four :: String
+four :: [String]
 four =
-  unlines
-    [ "#......",
-      "#......",
-      "#...#..",
-      "######.",
-      "....#.."
-    ]
+  [ "#......",
+    "#......",
+    "#...#..",
+    "######.",
+    "....#.."
+  ]
 
-five :: String
+five :: [String]
 five =
-  unlines
-    [ "######.",
-      "#......",
-      "######.",
-      ".....#.",
-      "######."
-    ]
+  [ "######.",
+    "#......",
+    "######.",
+    ".....#.",
+    "######."
+  ]
 
-six :: String
+six :: [String]
 six =
-  unlines
-    [ "######.",
-      "#......",
-      "######.",
-      "#....#.",
-      "######."
-    ]
+  [ "######.",
+    "#......",
+    "######.",
+    "#....#.",
+    "######."
+  ]
 
-seven :: String
+seven :: [String]
 seven =
-  unlines
-    [ "######.",
-      ".....#.",
-      ".....#.",
-      ".....#.",
-      ".....#."
-    ]
+  [ "######.",
+    ".....#.",
+    ".....#.",
+    ".....#.",
+    ".....#."
+  ]
 
-eight :: String
+eight :: [String]
 eight =
-  unlines
-    [ "######.",
-      "#....#.",
-      "######.",
-      "#....#.",
-      "######."
-    ]
+  [ "######.",
+    "#....#.",
+    "######.",
+    "#....#.",
+    "######."
+  ]
 
-nine :: String
+nine :: [String]
 nine =
-  unlines
-    [ "######.",
-      "#....#.",
-      "######.",
-      ".....#.",
-      "######."
+  [ "######.",
+    "#....#.",
+    "######.",
+    ".....#.",
+    "######."
+  ]
+
+numberToDigit :: Int -> [String]
+numberToDigit n =
+  case n of
+    0 -> zero
+    1 -> one
+    2 -> two
+    3 -> three
+    4 -> four
+    5 -> five
+    6 -> six
+    7 -> seven
+    8 -> eight
+    9 -> nine
+    _ -> error "Invalid digit"
+
+timeToDigitalLines :: [Int] -> [String]
+timeToDigitalLines [h1, h2, m1, m2] =
+  horizontalFlatten
+    [ numberToDigit h1,
+      numberToDigit h2,
+      colon,
+      numberToDigit m1,
+      numberToDigit m2
     ]
+timeToDigitalLines _ = error "Invalid time"
+
+-- flatten 2d array to 1d array horizontally
+-- [["a", "b"], ["c", "d"], ["e", "f"]] -> ["ace", "bdf"]
+horizontalFlatten :: [[String]] -> [String]
+horizontalFlatten xs = map concat (transpose xs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Lib.Digit
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  testDigits
+
+testDigits :: IO ()
+testDigits = hspec $ do
+  describe "Testing time to digital font" $ do
+    it "should return the correct string list for 12:34" $ do
+      let digits_str = timeToDigitalLines [1, 2, 3, 4]
+      let expect_str = [".....#.######...######.#......", ".....#......#.#......#.#......", ".....#.######......###.#...#..", ".....#.#......#......#.######.", ".....#.######...######.....#.."]
+      putStrLn $ "digits_str: \n" ++ (unlines digits_str)
+      digits_str `shouldBe` expect_str
